f0cbe93b5ded8092e0f290d0da166dd57b7edb1d
chandredewet <chandredewet@gmail.com>
2021-05-10 08:19:45 +0200

    Level 100 completed
    Shows All Episodes with specific detail (heading, image and summary). Then includes credits and styled accordingly.

--- script.js
+++ script.js
@@ -5,8 +5,62 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
+  //root is a new div in the html
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  //loop through all the episodes
+  episodeList.forEach((episode) => {
+    // First Display Episode Heading - format e.g Winter is Coming - S01E03
+    let episodeCard = document.createElement("div");
+    episodeCard.className = "sizeArea";
+    rootElem.appendChild(episodeCard);
+
+    let tvShowDetails = document.createElement("h2");
+    tvShowDetails.className = "epCardDetails";
+    tvShowDetails.innerText = `${
+      episode.name
+    } - S${episode.season
+      .toString()
+      .padStart(2, 0)}E${episode.number.toString().padStart(2, 0)}`;
+    episodeCard.appendChild(tvShowDetails);
+
+    //Add Episode Image to root div
+    let tvShowImage = document.createElement("img");
+    tvShowImage.className = "epCardImage";
+    tvShowImage.src = episode.image.medium;
+    episodeCard.appendChild(tvShowImage);
+
+    //Add Episode Summary to root div
+    let tvShowSummary = document.createElement("P");
+    tvShowSummary.className = "epCardSummary";
+    tvShowSummary.innerHTML = episode.summary;
+    episodeCard.appendChild(tvShowSummary);
+    //Loop ends here
+  });
+
+  //add a footer with details
+  let siteFooter = document.createElement("footer");
+
+  let footerParagraph = document.createElement("p");
+  footerParagraph.innerText =
+    "The data displayed on this page was originally taken from ";
+
+  let link = document.createElement("A");
+  link.innerText = "here";
+  link.setAttribute("href", "https://www.tvmaze.com/");
+
+  document.body.appendChild(siteFooter);
+  siteFooter.appendChild(footerParagraph);
+  footerParagraph.appendChild(link);
+  /*
+  let tvShowCredit = document.createElement("footer");
+  tvShowCredit.innerText = "The Data from this Site is taken from:";
+  document.body.appendChild(tvShowCredit);
+  let tvShowCredSite = document.createElement("a");
+  tvShowCredSite.src = "tvmaze.com/api#licensing";
+  tvShowCredit.appendChild(tvShowCredSite);
+
+  */
 }
 
 window.onload = setup;
