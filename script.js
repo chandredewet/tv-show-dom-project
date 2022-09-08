931647d6615d1fb7187474ddbcf0dd0c975950d4
chandredewet <chandredewet@gmail.com>
2022-09-08 23:02:30 +0200

    level 100 1, 2 - showing required info

--- script.js
+++ script.js
@@ -6,7 +6,30 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  episodeList.forEach((episode) => {
+    let articleEl = document.createElement("article");
+    articleEl.className = "movieCard";
+
+    //the episode's name, the season number, the episode number
+
+    let titleEl = document.createElement("h2");
+    titleEl.textContent += `${episode.name} S${episode.season} ${episode.number}`;
+    articleEl.append(titleEl);
+
+    // the episode's medium-sized image
+    let imgEl = document.createElement("img");
+    imgEl.src = episode.image.medium;
+    imgEl.alt = `Image of Game of Thrones Episode: S${episode.season} E${episode.number}`;
+    articleEl.append(imgEl);
+
+    // the episode's summary text
+    let summEl = document.createElement("p");
+    summEl.innerHTML = episode.summary;
+    articleEl.append(summEl);
+
+    rootElem.appendChild(articleEl);
+  });
 }
 
 window.onload = setup;
