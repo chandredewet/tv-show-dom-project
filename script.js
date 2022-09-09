1aca1c10cf8a80df35f074564289d1dd371fae8d
chandredewet <chandredewet@gmail.com>
2022-09-09 02:45:51 +0200

    level 100 all completed, script and styling

--- script.js
+++ script.js
@@ -7,14 +7,48 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
 
+  let cardColours = [
+    "#e2e2df",
+    "#d2d2cf",
+    "#e2cfc4",
+    "#f7d9c4",
+    "#faedcb",
+    "#c9e4de",
+    "#c6def1",
+    "#dbcdf0",
+    "#f2c6de",
+    "#f9c6c9",
+  ];
+
+  let headingEl = document.createElement("div");
+  headingEl.innerText = "tv show project";
+  headingEl.className = "headingEl";
+  rootElem.appendChild(headingEl);
+
+  let count = 0;
+
+  const showsEl = document.createElement("section");
+  showsEl.className = "showsEl";
+  rootElem.appendChild(showsEl);
+
   episodeList.forEach((episode) => {
     let articleEl = document.createElement("article");
+    articleEl.style.backgroundColor = cardColours[count];
+    let titleEl = document.createElement("h2");
+
+    if (count < cardColours.length - 1) {
+      count++;
+    } else {
+      count = 0;
+    }
     articleEl.className = "movieCard";
 
     //the episode's name, the season number, the episode number
+    let episodeCode =
+      episode.season.toString().padStart(2, "0") +
+      episode.number.toString().padStart(2, "0");
 
-    let titleEl = document.createElement("h2");
-    titleEl.textContent += `${episode.name} S${episode.season} ${episode.number}`;
+    titleEl.textContent += `${episode.name} - S${episodeCode}`;
     articleEl.append(titleEl);
 
     // the episode's medium-sized image
@@ -28,8 +62,14 @@ function makePageForEpisodes(episodeList) {
     summEl.innerHTML = episode.summary;
     articleEl.append(summEl);
 
-    rootElem.appendChild(articleEl);
+    showsEl.appendChild(articleEl);
   });
+
+  let creditEl = document.createElement("p");
+  creditEl.innerHTML =
+    'The data in this website originally comes from <a href="https://www.tvmaze.com/">TVMaze.com</a>';
+  creditEl.className = "credit";
+  rootElem.appendChild(creditEl);
 }
 
 window.onload = setup;
